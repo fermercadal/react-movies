2a6126fdf5ee3fb0257d4e474b128c2de9dfa209
Fernando Mercadal <fermercadal@outlook.com>
2021-10-24 08:58:14 -0300

    Adding state reports

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import "./App.scss";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState();
 
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -20,6 +22,7 @@ function App() {
     });
 
     setMovies(serializedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -28,7 +31,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies yet.</p>}
+        {isLoading && <p>Loading..</p>}
       </section>
     </React.Fragment>
   );
